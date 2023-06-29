141624912fe83bee7546a175405baa5ca7249b02
Anand <akkeshri14042001@gmail.com>
2023-06-29 13:22:31 +0530

    first commit

---
+++ public/src/App.js
@@ -0,0 +1,20 @@
+import React, { lazy,Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const Chat=lazy(()=>import("./pages/Chat"))
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const SetAvatar = lazy(() => import("./components/SetAvatar"));
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/setAvatar" element={<SetAvatar />} />
+          <Route path="/" element={<Chat />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
